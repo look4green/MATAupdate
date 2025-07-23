f9e272dc6b55c9f48b284e909a00a8323c16ec6c
look4green <look4green@users.noreply.github.com>
2025-07-23 17:13:12 +0800

    Update AppState.cpp

--- MATA/AppState.cpp
+++ MATA/AppState.cpp
@@ -1,15 +1,10 @@
-#include <Arduino.h>
 #include "AppState.h"
 
-// ✅ Define globals here, only once
+// Global variable definitions
 AppState currentState = HOME;
 int menuIndex = 0;
-
-void setAppState(AppState newState) {
-  currentState = newState;
-}
-
 const int menuSize = 6;
+
 String menuItems[menuSize] = {
   "Status",
   "Sniff Packets",
@@ -18,3 +13,7 @@ String menuItems[menuSize] = {
   "Replay",
   "Diagnostics"
 };
+
+void setAppState(AppState newState) {
+  currentState = newState;
+}
